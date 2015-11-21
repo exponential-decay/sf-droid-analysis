3b185b69ff6eaeeef4bd8d17cb94b997fa41e42e
ross-spencer <all.along.the.watchtower2001+github@gmail.com>
2015-11-21 19:33:48 +1300

    [feature] CLI handling basics.

--- sf-droid-analysis.go
+++ sf-droid-analysis.go
@@ -2,23 +2,58 @@
 package main
 
 import (
-   "fmt"
+   //"fmt"
    "os"
    "github.com/codegangsta/cli"
 )
 
 func main() {
+
    app := cli.NewApp()
+
    app.Name = "sf-droid-analysis"
    app.Usage = "Automated analysis of DROID and Siegfried reports for Digital Preservation."
    app.HelpName = "sf-droid-analysis"
    app.Version = "0.1.0"
    app.Author = "exponential-decay"
+
+   var rogues bool
+   var report string
+   var database string
+
+   app.Flags = []cli.Flag {
+      cli.StringFlag{
+         Name:          "report, csv, csva",
+         Usage:         "Droid or Siegfried report to analyse.",
+         Destination:   &report,
+      }, 
+      cli.StringFlag{
+         Name:          "db",
+         Usage:         "Database to output results from.",
+         Destination:   &database,
+      },     
+      cli.BoolFlag{
+         Name:          "rogues, rogue",
+         Usage:         "Output Rogues Gallery",
+         Destination:   &rogues,
+      },
+      cli.StringFlag{
+         Name:          "export",
+         Usage:         "Export database as CSV.",
+         Destination:   &database,
+      },
+   }
+
    app.Action = func(c *cli.Context) {
-      if len(c.Args()) < 2 {
+      if len(os.Args) < 2 {
          cli.ShowAppHelp(c)
       } else {
-         fmt.Println(c.Args())
+         if len(c.Args()) > 0 {
+            //we can do things with string flagss
+         }
+         if rogues == true {
+            //handle rogue output
+         }
       }
    }
 
