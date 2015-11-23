3deea22bee7dd62bc7fdc46930d531b774c1acd7
ross-spencer <all.along.the.watchtower2001+github@gmail.com>
2015-11-23 21:30:41 +1300

    [ignore] Structure for SQLITE work.

--- build-analysis-db.go
+++ build-analysis-db.go
@@ -3,6 +3,7 @@
 package main
 
 import (
+   "log"
    "github.com/mattn/go-sqlite3"
    "database/sql"
 )
@@ -12,5 +13,12 @@ func dostuff() error {
    var DB_DRIVER string
    sql.Register(DB_DRIVER, &sqlite3.SQLiteDriver{})
 
+   _, err := sql.Open(DB_DRIVER, "mysqlite_3") 
+   if err != nil {
+      log.Println("ERROR: Failed to create the handle.")
+   }
+
+
    return nil
 }
+
